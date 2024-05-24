7d4e92927a844181a61f9330a0b8a0e92f3f4389
JasonChamorro <jchamor1@lion.lmu.edu>
2024-05-24 16:47:49 -0700

    Initial Commit

---
+++ FirstDraftApp/FirstDraftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstDraftAppApp.swift
+//  FirstDraftApp
+//
+//  Created by Jason Chamorro on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstDraftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
